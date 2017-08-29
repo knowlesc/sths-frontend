d71b2ed4d395df00b6e3e04abecd6e6a56e7ab07
knowlesc <knowlesc8@gmail.com>
2017-08-28 23:39:10 -0700

    Allow all-star game boxscores

--- src/views/boxscore/boxscore.controller.ts
+++ src/views/boxscore/boxscore.controller.ts
@@ -7,7 +7,7 @@ export class BoxscoreController {
 
   static $inject = ['$routeParams', 'config'];
   constructor($routeParams: ng.route.IRouteParamsService, config: Config) {
-    if ($routeParams['id'] && /^\w+(-Farm)?-\d+\.html$/.test($routeParams['id'])) {
+    if ($routeParams['id'] && /^\w+((-Farm-)|(-Exh)|(-))\d+\.html$/.test($routeParams['id'])) {
       this.boxscoreUrl = `${config.boxscorePath}${$routeParams['id']}`;
       this.fail = false;
     }
